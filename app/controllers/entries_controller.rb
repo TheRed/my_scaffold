403d89dc4955e0e614b22862b50124e3587d90a6
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-01-30 16:53:14 +0900

    何も見ずに書ける限りで書いた。。。

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,9 +1,36 @@
 class EntriesController < ApplicationController
+  before_action :get_entry, only: [:show, :edit]
+
   def index
     @entries = Entry.all
   end
 
   def show
-    @entry = Entry.find(params[:id])
   end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def edit
+  end
+
+  def create
+    @entry = Entry.new
+
+    if @entry.save
+      redirect_to entries_path
+    end
+  end
+
+  def update
+    if @entry.update
+      redirect_to entry_path
+    end
+  end
+
+  private
+    def get_entry
+      @entry = Entry.find(params[:id])
+    end
 end
