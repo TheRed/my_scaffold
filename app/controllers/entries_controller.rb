b6a6d525223e2679f02f7400eb7ad9b25a76c758
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-01-31 10:06:32 +0900

    showページ作成

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,15 @@
 class EntriesController < ApplicationController
+  before_action :set_entry, only: [:show]
+
   def index
     @entries = Entry.all
   end
+
+  def show
+  end
+
+  private
+    def set_entry
+      @entry = Entry.find(params[:id])
+    end
 end
