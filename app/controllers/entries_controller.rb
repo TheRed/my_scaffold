491ef07163dafc84673c93bf85c93d1778eef766
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-02-01 05:41:31 +0900

    コントローラの基本メソッド作成

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,31 @@
 class EntriesController < ApplicationController
+  before_action :set_entry, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def set_entry
+    end
+
+    def entry_params
+    end
 end
