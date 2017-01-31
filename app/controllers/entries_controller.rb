b366717e4adaaa2ccec61fe407ffe80dfbcd07e1
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-01-31 15:25:47 +0900

    コントローラの基本形を作成

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,34 @@
 class EntriesController < ApplicationController
+  before_action :set_entry, only: [:show, :edit, :update, :destroy]
+
   def index
     @entries = Entry.all
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def set_entry
+      @entry = Entry.find(params[:id])
+    end
+
+    def entry_params
+      params.require(:entry).permit(:title, :body)
+    end
 end
